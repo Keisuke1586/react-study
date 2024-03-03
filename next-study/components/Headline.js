0986e49220c9f7a818b69875964a75fbb3e0fddf
Keisuke1586 <fushanguifu@gmail.com>
2024-03-04 00:02:57 +0900

    ♻️ export defaultの削除

--- next-study/components/Headline.js
+++ next-study/components/Headline.js
@@ -1,5 +1,5 @@
 import styles from "@/styles/Home.module.css";
-export default function Headline(props) {
+export  function Headline(props) {
     return (
         <div className={styles.center}>
             <h1 className={styles.text}>{props.page} Page</h1>
